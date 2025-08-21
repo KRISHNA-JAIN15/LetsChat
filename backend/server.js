6cb915e31fa671ed218fcb8789e2a75aa192dbe8
KRISHNA-JAIN15 <krishna.j23@iiits.in>
2025-08-21 21:17:33 +0530

    Domain

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000
 // const __dirname = path.resolve()
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://lets-chat-ivory.vercel.app', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'https://lets-chat-ivory.vercel.app', 'http://localhost:5173', 'https://letsmeet.krishnajain.tech'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
